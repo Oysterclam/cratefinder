1baea2e0cd4a2cca730a4d261adb62d8ef2db9eb
Oysterclam <andyy219@gmail.com>
2018-07-30 15:18:17 -0700

    ok it matches better now

--- alsodelete.py
+++ alsodelete.py
@@ -15,9 +15,10 @@ arr=[]
 for a in range(0,3):
     for b in range(0,3):
         arr.append(img1[int(a*h/3):int((a+1)*h/3),int(b*w/3):int((b+1)*w/3)])
-MIN_MATCH_COUNT = 7
+MIN_MATCH_COUNT = 4
 imgarray = []
 kp=[]
+
 realgood=[]
 
 for a in range(0,9):
@@ -25,13 +26,11 @@ for a in range(0,9):
  
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
- 
+
     FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
-    #print(matches)
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2, k=2)
+        #print(matches)
 
     good = []
     for m,n in matches:
@@ -39,17 +38,16 @@ for a in range(0,9):
             good.append(m)
             #print(m)
 
-    if len(good)>=MIN_MATCH_COUNT:
-        realgood.extend(good)
-        print('hi')
-        print(realgood)
+    if len(good)>=1:
+        print(len(good))
+       
         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
             
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
         matchesMask = mask.ravel().tolist()
-        h,w = img2.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        # h,w = img2.shape
+        # pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
         # dst = cv2.perspectiveTransform(pts,M)
         # img1 = cv2.polylines(img1,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 
@@ -71,12 +69,20 @@ for a in range(0,9):
                        matchesMask = matchesMask, 
                        flags = 2)    
     img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)  
-    
-    h,w=img1.shape[0:2]
-    
+    gg=np.array([1,2,3,4,5])
+ 
+    print(len(kp))
+    goodtemp=good
+    for match in goodtemp:
+        match.queryIdx+=len(kp)-len(kp1)
 
+    realgood.extend(goodtemp)
+
+        
+  
+    h,w=img1.shape[0:2]
     plt.imshow(img3, 'gray'),plt.show()
-    imgarray.append(img3[0:int(h),0:int(w)])
+    imgarray.append(img1[0:int(h),0:int(w)])
 
 
 row1=np.concatenate((imgarray[0], imgarray[1],imgarray[2]), axis=1)
@@ -85,9 +91,10 @@ row3=np.concatenate((imgarray[6], imgarray[7],imgarray[8]), axis=1)
 vis = np.concatenate((row1,row2,row3),axis=0)
 ee = cv2.drawKeypoints(vis,kp,None)
 FLANN_INDEX_KDTREE = 1
-
+for match in realgood:
+    print(match.queryIdx)
 #plt.imshow(ee,'gray')
-plt.show()
+#plt.show()
 vis = cv2.drawMatches(vis,kp,img2,kp2,realgood,None)  
 plt.imshow(vis, 'gray'),plt.show()
 
